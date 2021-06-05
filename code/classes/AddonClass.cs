89104d39bdb4dbb8bfe42aaeddaea218ee385796
Alex Casasola <alex@casasola.dev>
2021-06-05 12:01:48 -0700

    S&Box did not appreciate AddonClass being abstract; removing it for now.

--- code/classes/AddonClass.cs
+++ code/classes/AddonClass.cs
@@ -1,27 +1,23 @@
 ﻿using Sandbox;
 using System;
 
-namespace MinimalExtended.classes
+/// <summary>
+/// Used to dictate that a C# file is an addon. Should be the entrypoint of your addon.
+/// Derives from LibraryClass, and manages the Event lifecycle for you.
+/// </summary>
+public class AddonClass : LibraryClass
 {
-  /// <summary>
-  /// Used to dictate that a C# file is an addon. Should be the entrypoint of your addon.
-  /// Derives from LibraryClass, and manages the Event lifecycle for you.
-  /// </summary>
-  public abstract class AddonClass : LibraryClass
+  public static bool IsServer => Host.IsServer;
+  public static bool IsClient => Host.IsClient;
+  public AddonClass()
   {
-    public static bool IsServer => Host.IsServer;
-    public static bool IsClient => Host.IsClient;
-    public AddonClass()
-    {
-      Event.Register( this );
-    }
-
-    ~AddonClass()
-    {
-      Event.Unregister( this );
-    }
-
-    public virtual void Register() { }
+    Event.Register( this );
   }
 
+  ~AddonClass()
+  {
+    Event.Unregister( this );
+  }
+
+  public virtual void Register() { }
 }
